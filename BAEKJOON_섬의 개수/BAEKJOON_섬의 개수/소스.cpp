8bf01dd23a5cfb790dba8568fb36148bbe679a15
ChangminYi <jerry9803@gmail.com>
2021-06-20 20:32:44 +0900

    No. 4963 Solved

--- BAEKJOON_섬의 개수/BAEKJOON_섬의 개수/소스.cpp
+++ BAEKJOON_섬의 개수/BAEKJOON_섬의 개수/소스.cpp
@@ -4,22 +4,18 @@
 
 using namespace std;
 
-#define MAX 50
-
-const vector<pair<int, int>> move = {
+vector<pair<int, int>> dir = {
 	{0, 1}, {0, -1}, { 1, 0}, {-1, 0},
 	{1, 1}, {1, -1}, {-1, 1}, {-1, -1}
 };
-queue<pair<int, int>> togo;
-vector<vector<bool>> visit(MAX, vector<bool>(MAX));
-vector<vector<int>> map(MAX, vector<int>(MAX));
-int r = 0, c = 0, island = 0, i = 0, j = 0;
-uint32_t k = 0;
-pair<int, int> range, cur, temp;
 
-bool inRange(const pair<int, int> &a, const pair<int, int> &range) {
-	if (0 <= a.first && a.first < range.first) {
-		if (0 <= a.second && a.second < range.second) {
+int map[50][50] = { 0 };
+bool visit[50][50] = { false };
+int col = 0, row = 0;
+
+bool inRange(const pair<int, int> &pos) {
+	if (0 <= pos.first && pos.first < row) {
+		if (0 <= pos.second && pos.second < col) {
 			return true;
 		}
 	}
@@ -28,51 +24,53 @@ bool inRange(const pair<int, int> &a, const pair<int, int> &range) {
 
 int main() {
 	cin.tie(nullptr);
-	cout.tie(nullptr);
 	cin.sync_with_stdio(false);
+	cout.tie(nullptr);
 	cout.sync_with_stdio(false);
-
+	
+	queue<pair<int, int>> togo;
+	
 	while (true) {
-		cin >> c >> r;
-		if (r == 0 && c == 0) {
+		int cnt = 0;
+		cin >> col >> row;
+		
+		if (col == 0 && row == 0) {
 			break;
 		}
 		else {
-			for (i = 0; i < r; i++) {
-				for (j = 0; j < c; j++) {
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
 					cin >> map[i][j];
 					visit[i][j] = false;
 				}
 			}
 
-			island = 0;
-			range.first = r;
-			range.second = c;
-			for (i = 0; i < r; i++) {
-				for (j = 0; j < c; j++) {
-					if (map[i][j] == 1 && !visit[i][j]) {
-						island++;
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
 
-						togo.push(make_pair(i, j));
+					if (map[i][j] == 1 && !visit[i][j]) {
+						cnt++;
+						togo.push({ i, j });
+						visit[i][j] = true;
 						while (!togo.empty()) {
-							cur.first = togo.front().first;
-							cur.second = togo.front().second;
-							visit[cur.first][cur.second] = true;
+							pair<int, int> cur = togo.front();
 							togo.pop();
 
-							for (k = 0; k < ::move.size(); k++) {
-								temp.first = cur.first + ::move[k].first;
-								temp.second = cur.second + ::move[k].second;
-								if (inRange(temp, range) && map[temp.first][temp.second] == 1 && !visit[temp.first][temp.second]) {
-									togo.push(temp);
+							for (const pair<int, int>& t : dir) {
+								pair<int, int> tmp = make_pair(t.first + cur.first, t.second + cur.second);
+								
+								if (inRange(tmp) && map[tmp.first][tmp.second] == 1 && !visit[tmp.first][tmp.second]) {
+									visit[tmp.first][tmp.second] = true;
+									togo.push(tmp);
 								}
 							}
 						}
 					}
+
 				}
 			}
 
-			cout << island << '\n';
+			cout << cnt << '\n';
 		}
 	}
 
