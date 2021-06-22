0a099b2c66fdc4c252db52aeddc77d7e79b07c4e
ChangminYi <jerry9803@gmail.com>
2021-06-22 22:30:12 +0900

    No. 4355 solved

--- BAEKJOON_서로소/BAEKJOON_서로소/소스.cpp
+++ BAEKJOON_서로소/BAEKJOON_서로소/소스.cpp
@@ -1,13 +1,10 @@
-//시간초과
-
 #include <iostream>
+#include <deque>
+#include <cmath>
+#include <algorithm>
 
+#define MAX 1000000000
 using namespace std;
-
-int gcd(int a, int b) {
-	if (b == 0) { return a; }
-	else { return gcd(b, a % b); }
-}
 
 int main() {
 	cin.tie(nullptr);
@@ -15,20 +12,53 @@ int main() {
 	cout.tie(nullptr);
 	cout.sync_with_stdio(false);
 
-	int n = 0;
+	deque<int> prime = { 2, 3 };
+	bool isDivisible = false;
+	for (int i = 5; i <= sqrt(MAX); i += 2) {
+		for (size_t j = 0; prime[j] <= sqrt(i); j++) {
+			if (i % prime[j] == 0) {
+				isDivisible = true;
+				break;
+			}
+		}
+
+		if (!isDivisible) {
+			prime.push_back(i);
+		}
+		isDivisible = false;
+	}
+
 	while (true) {
+		int n = 0;
 		cin >> n;
+
 		if (n == 0) {
 			break;
 		}
 		else {
-			int counter = 1;
-			for (int i = 2; i < n; i++) {
-				if (gcd(n, i) == 1) { counter++; }
+			int res = 1;
+			pair<int, int> temp = make_pair(0, 0);
+			int to_comp = n;
+			for (size_t i = 0; to_comp > 1 && i < prime.size(); i++) {
+				if (to_comp % prime[i] == 0) {
+					temp = make_pair(prime[i], 0);
+					
+					while (to_comp % prime[i] == 0) {
+						to_comp /= prime[i];
+						temp.second++;
+					}
+					res *= (int)pow(temp.first, temp.second - 1) * (temp.first - 1);
+				}
 			}
-			cout << counter << '\n';
+
+			if (to_comp != 1) {
+				res *= to_comp - 1;
+			}
+
+			cout << res << '\n';
 		}
 	}
+
 
 	return 0;
 }
