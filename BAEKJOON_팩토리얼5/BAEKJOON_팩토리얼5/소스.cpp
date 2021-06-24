ed6ec6bdf1233ba4b5ef21c25cfcd9319b15e5ff
ChangminYi <jerry9803@gmail.com>
2021-06-24 16:34:22 +0900

    No. 1564 solved

--- BAEKJOON_팩토리얼5/BAEKJOON_팩토리얼5/소스.cpp
+++ BAEKJOON_팩토리얼5/BAEKJOON_팩토리얼5/소스.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
-#include <string>
+#include <iomanip>
 
 using namespace std;
 
-int main(){
+#define MAX 1000000000000
+typedef unsigned long long ulli;
+
+int main() {
 	cin.tie(nullptr);
 	cin.sync_with_stdio(false);
-	
-	int n = 0;
-	cin >> n;
+	cout.tie(nullptr);
+	cout.sync_with_stdio(false);
 
-	long long s = 1, t = 10000000;
-	for (long long i = 1, j = 1; i <= n; i++) {
-		for (j = 1; (j <= s * i) && (s * i / j % 10 == 0); j *= 10);
-		s = s * i / j % t;
-	}
-	
-	s %= 100000;
-	string out = "00000";
-	for (int i = 4; i >= 0; i--) {
-		out[i] += s % 10;
-		s /= 10;
+	int N = 0;
+	ulli res = 1;
+
+	cin >> N;
+
+	for (ulli i = N; i >= 1; i--) {
+		res *= i;
+		while (res % 10 == 0) {
+			res /= 10;
+		}
+
+		res %= MAX;
 	}
 
-	cout << out;
+	res %= 100000;
+	cout.width(5);
+	cout.fill('0');
+	cout << res;
 
 	return 0;
 }
