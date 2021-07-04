41ca7fe26e779126d8985d46dd797f8fd3496d23
ChangminYi <jerry9803@gmail.com>
2021-07-04 12:17:17 +0900

    No. 2887 solved

--- BAEKJOON_행성 터널/BAEKJOON_행성 터널/소스.cpp
+++ BAEKJOON_행성 터널/BAEKJOON_행성 터널/소스.cpp
@@ -1,57 +1,120 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <cmath>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 
-typedef struct COORDINATE {
-	int x, y, z;
-}cor;
+typedef struct NODE {
+    int x, y, z;
+    int idx;
+}node;
 
-int distance(const cor &a, const cor &b) {
-	return min(abs(a.x - b.x), min(abs(a.y - b.y), abs(a.z - b.z)));
+typedef struct EDGE {
+    int i, j, weight;
+    bool operator< (const EDGE& comp) const {
+        return this->weight < comp.weight;
+    }
+}edge;
+
+vector<node> graph;
+vector<edge> route;
+vector<int> parent;
+
+bool comp_x(const node& a, const node& b) {
+    return a.x < b.x;
 }
 
-long long prim(vector<cor> &graph) {
-	long long sum = 0;
-	vector<bool> visit(graph.size(), false);
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+bool comp_y(const node& a, const node& b) {
+    return a.y < b.y;
+}
 
-	pq.push({ 0, 0 });
-	while (!pq.empty()) {
-		int curCost = pq.top().first;
-		int curPos = pq.top().second;
-		pq.pop();
+bool comp_z(const node& a, const node& b) {
+    return a.z < b.z;
+}
 
-		if (!visit[curPos]) {
-			sum += curCost;
-			visit[curPos] = true;
+int _find(int x) {
+    if (parent[x] == x) {
+        return x;
+    }
+    else {
+        return parent[x] = _find(parent[x]);
+    }
+}
 
-			for (uint32_t i = 0; i < graph.size(); i++) {
-				if (!visit[i]) {
-					pq.push({distance(graph[curPos], graph[i]), i});
-				}
-			}
-		}
-	}
+bool is_same_set(int x, int y) {
+    return _find(x) == _find(y);
+}
 
-	return sum;
+void _union(int x, int y) {
+    int p_x = _find(x), p_y = _find(y);
+
+    if (p_x < p_y) {
+        parent[p_y] = p_x;
+    }
+    else {
+        parent[p_x] = p_y;
+    }
+}
+
+int get_dist(int i, int j) {
+    return abs(i - j);
 }
 
 int main() {
-	cin.tie(nullptr);
-	cin.sync_with_stdio(false);
+    cin.tie(0);
+    cin.sync_with_stdio(false);
+    cout.tie(nullptr);
+    cout.sync_with_stdio(false);
+    
+    int n;
+    cin >> n;
+    
+    parent = vector<int>(n);
+    graph = vector<node>(n);
+    for (int i = 0; i < n; i++) {
+        cin >> graph[i].x >> graph[i].y >> graph[i].z;
+        graph[i].idx = i;
+        parent[i] = i;
+    }
 
-	int n = 0;
-	cin >> n;
-	vector<cor> planet(n);
-	for (cor &i : planet) {
-		cin >> i.x >> i.y >> i.z;
-	}
+    edge tmp;
+    int tmp_dist;
+    sort(graph.begin(), graph.end(), comp_x);
+    for (int i = 0; i < n - 1; i++) {
+        tmp_dist = get_dist(graph[i].x, graph[i + 1].x);
+        tmp = { graph[i].idx, graph[i + 1].idx, tmp_dist };
+        route.push_back(tmp);
+    }
+    sort(graph.begin(), graph.end(), comp_y);
+    for (int i = 0; i < n - 1; i++) {
+        tmp_dist = get_dist(graph[i].y, graph[i + 1].y);
+        tmp = { graph[i].idx, graph[i + 1].idx, tmp_dist };
+        route.push_back(tmp);
+    }
+    sort(graph.begin(), graph.end(), comp_z);
+    for (int i = 0; i < n - 1; i++) {
+        tmp_dist = get_dist(graph[i].z, graph[i + 1].z);
+        tmp = { graph[i].idx, graph[i + 1].idx, tmp_dist };
+        route.push_back(tmp);
+    }
+    sort(route.begin(), route.end());
+    
+    long long cost = 0;
+    int cnt = 0;
+    for (edge& e : route) {
+        if (!is_same_set(e.i, e.j)) {
+            cost += e.weight;
+            _union(e.i, e.j);
+            cnt++;
 
-	cout << prim(planet);
+            if (cnt == n - 1) {
+                break;
+            }
+        }
+    }
 
-	return 0;
+    cout << cost;
+
+    return 0;
 }
