9e6026ab138517c1a626e81f040d87ffba5b466f
ChangminYi <jerry9803@gmail.com>
2021-07-15 10:24:35 +0900

    No. 10422 solved

--- BAEKJOON_괄호/BAEKJOON_괄호/소스.cpp
+++ BAEKJOON_괄호/BAEKJOON_괄호/소스.cpp
@@ -1,37 +1,42 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
+
 using namespace std;
 
+typedef long long lli;
+
+const lli DIV = 1000000007;
+const int MAX_SIZE = 2500;
+
+vector<lli> dp(MAX_SIZE + 1, 0);
+
 int main() {
-	cin.tie(NULL);
+	cin.tie(nullptr);
 	cin.sync_with_stdio(false);
-	int size = 0;
-	cin >> size;
-	for (; size > 0; size--) {
-		stack<char> stk;
-		string lang;
-		cin >> lang;
+	cout.tie(nullptr);
+	cout.sync_with_stdio(false);
 
-		for (int i = 0; i < lang.size(); i++) {
-			if (lang[i] == '(') {
-				stk.push('(');
-			}
-			else if (lang[i] == ')') {
-				if (stk.empty()) {
-					cout << "NO\n";
-					break;
-				}
-				else {
-					stk.pop();
-				}
-			}
-
-			if (i == lang.size() - 1) {
-				if (stk.empty()) { cout << "YES\n"; }
-				else { cout << "NO\n"; }
-			}
+	dp[0] = 1;
+	for (int i = 1; i <= MAX_SIZE; i++) {
+		for (int j = 0; j < i; j++) {
+			dp[i] += dp[j] * dp[i - j - 1] % DIV;
 		}
+		dp[i] %= DIV;
 	}
+
+	int iter;
+	cin >> iter;
+	do {
+		int n;
+		cin >> n;
+
+		if ((n & 1) == 1) {
+			cout << 0 << '\n';
+		}
+		else {
+			cout << dp[n / 2] << '\n';
+		}
+	} while (--iter > 0);
+
 	return 0;
 }
