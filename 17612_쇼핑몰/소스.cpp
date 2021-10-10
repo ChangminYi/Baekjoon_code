1516e5c3d4820ca6e50f9f526fa90ef8ca320d60
ChangminYi <jerry9803@gmail.com>
2021-10-10 21:24:18 +0900

    No. 17612 re-solved: code refactoring

--- 17612_쇼핑몰/소스.cpp
+++ 17612_쇼핑몰/소스.cpp
@@ -7,88 +7,81 @@ using namespace std;
 
 typedef long long lli;
 typedef pair<int, int> pint;
-struct comp {
-    bool operator()(pint& lhs, pint& rhs) {
-        return (lhs.first == rhs.first ? lhs.second < rhs.second : lhs.first > rhs.first);
+struct pq_n {
+    int line_n = 0, out_time = 0, cus_id = 0;
+    pq_n(int line, int time, int id) : line_n(line), out_time(time), cus_id(id) {}
+    bool operator>(const pq_n& rhs) const {
+        if (this->out_time == rhs.out_time) {
+            return this->line_n < rhs.line_n;
+        }
+        return this->out_time > rhs.out_time;
     }
 };
 
-int n, k, cus_id, item_n, line_id, wait_t;
-lli res;
-bool all_lined = false;
-vector<queue<pint>> lines;
-priority_queue<pint, vector<pint>, greater<pint>> min_pq;
-priority_queue<pint, vector<pint>, comp> out_pq;
+lli n, k, res;
+vector<queue<pint>> line;
+priority_queue<pint, vector<pint>, greater<pint>> line_out_pq;
+priority_queue<pq_n, vector<pq_n>, greater<pq_n>> total_out_pq;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
     cin >> n >> k;
-    lines.resize(k);
 
     /*
-    * 일단 낮은 번호의 계산대부터 한 명씩 넣는다.
-    * n명을 다 넣었으면 break한다.
-    * 그리고 빠져나가는 순서를 pq에 넣는다.
+    * 사용 가능한 계산대 줄 만들기
+    * 직관적으로 하기 위해서 1-base로 설정
     */
-    for (int i = 0; i < k; i++) {
-        if (i >= n) {
-            all_lined = true;
-            break;
-        }
-        else {
-            cin >> cus_id >> item_n;
-            lines[i].push(pint(item_n, cus_id));
-            min_pq.push(pint(item_n, i));
+    line.resize(k + 1);
+    for (int i = 1; i <= k; i++) {
+        line_out_pq.push(pint(0, i));
+    }
+    
+    /*
+    * 차례대로 들어오는 사람의 계산대 줄 설정
+    * line_out_pq의 top()은 조건 만족하는 가장 빠른 계산대
+    * 해당 번호의 큐에 저장하고, line_out_pq 업데이트
+    */
+    for (int i = 0, c_id, itm; i < n; i++) {
+        cin >> c_id >> itm;
+
+        int base_time = line_out_pq.top().first;
+        int line_idx = line_out_pq.top().second;
+        line_out_pq.pop();
+
+        line[line_idx].push(pint(c_id, base_time + itm));
+        line_out_pq.push(pint(base_time + itm, line_idx));
+    }
+
+    /*
+    * total_out_pq 초기화
+    * 각 계산대의 첫 번째 줄 사람들 total_out_pq에 삽입
+    */
+    for (int i = 1; i <= k; i++) {
+        if (!line[i].empty()) {
+            total_out_pq.push(pq_n(i, line[i].front().second, line[i].front().first));
+            line[i].pop();
         }
     }
 
     /*
-    * 아직 계산대에 들어가지 못한 사람이 남아있는 경우.
-    * 먼저 사람이 빠져나가는 계산대에 다음 사람은 넣는다.
+    * 1번부터 n번의 사람들 모두 체크
+    * 현재 나가는 사람의 계산대 줄 번호를 가져와서
+    * 그 줄에서 계산 끝난 사람의 정보 push
+    * 사람이 n명 있으니 n번만 반복하면 된다.
     */
-    if (!all_lined) {
-        for (int i = k; i < n; i++) {
-            cin >> cus_id >> item_n;
+    for (lli i = 1; i <= n; i++) {
+        int c_id = total_out_pq.top().cus_id;
+        int cur_line = total_out_pq.top().line_n;
+        total_out_pq.pop();
 
-            line_id = min_pq.top().second;
-            wait_t = min_pq.top().first + item_n;
-            min_pq.pop();
+        res += c_id * i;
+        if (!line[cur_line].empty()) {
+            int new_cus_id = line[cur_line].front().first;
+            int new_out_time = line[cur_line].front().second;
+            line[cur_line].pop();
 
-            lines[line_id].push(pint(wait_t, cus_id));
-            min_pq.push(pint(wait_t, line_id));
-        }
-    }
-
-    /*
-    * 나가는 순서를 정한다.
-    * 각 계산대에서 첫 번째로 계산을 마치는 사람들을 저장.
-    * 만약 최대 인원 넘어가면 끝낸다.
-    */
-    for (int i = 0; i < k; i++) {
-        if (i >= n) {
-            break;
-        }
-        else {
-            out_pq.push(pint(lines[i].front().first, i));
-        }
-    }
-
-    /*
-    * 결과 값 계산 및 남은 사람 다시 pq에 넣기
-    * 결과값에 넣은 사람의 줄 번호에 아직 사람이 남아있으면
-    * 그 사람을 pq에 넣어서 끝날 때까지 계산한다.
-    * 오버플로우 조심
-    */
-    for (lli i = 1; i <= (lli)n; i++) {
-        line_id = out_pq.top().second;
-        out_pq.pop();
-        cus_id = lines[line_id].front().second;
-        lines[line_id].pop();
-
-        res += i * cus_id;
-        if (!lines[line_id].empty()) {
-            out_pq.push(pint(lines[line_id].front().first, line_id));
+            total_out_pq.push(pq_n(cur_line, new_out_time, new_cus_id));
         }
     }
 
