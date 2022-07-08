d0ff15b8d780f062bee73b7cadd3d3dacf3c7baa
ChangminYi <jerry9803@gmail.com>
2022-07-08 09:47:42 +0900

    No. 25307 re-solved

--- 25307_시루의 백화점 구경/main.cpp
+++ 25307_시루의 백화점 구경/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cmath>
 
 using namespace std;
 
@@ -13,75 +12,80 @@ struct dir {
     }
 };
 
-struct qn {
-    dir pos;
-    int dist;
-    qn(const dir _p, const int _d) : pos(_p), dist(_d) {}
-};
-
+const int NOT_VISIT = INT32_MAX;
 const vector<dir> poss_dir = {
-    dir(1, 0), dir(-1, 0), dir(0, 1), dir(0, -1)
+    dir(0, 1), dir(0, -1), dir(1, 0), dir(-1, 0)
 };
 
 int n, m, k;
+int board[2000][2000], dist[2000][2000], mane_dist[2000][2000];
 dir sp;
-queue<qn> que;
-vector<dir> mannequin;
-int board[2000][2000];
-bool visit[2000][2000];
+queue<dir> que;
 
-bool in_range(const dir &d) {
+bool in_range(const dir& d) {
     return 0 <= d.row && d.row < n && 0 <= d.col && d.col < m;
-}
-
-bool far_manne(const dir &d) {
-    int min_dist = INT32_MAX;
-    for(const dir& m: mannequin) {
-        int diff = abs(d.row - m.row) + abs(d.col - m.col);
-        if(diff < min_dist)
-            min_dist = diff;
-    }
-    return min_dist > k;
 }
 
 int main() {
     /**
-     * bfs를 통해 해결할 수 있다. 마네킹과의 최소 거리를 지키는 지를
-     * 판단하면서 탐색하면 된다.
+     * 마네킹과의 거리를 미리 bfs로 구한다.
+     * 그 뒤, 시작점에서 bfs를 하여 의자까지의 최단거리를 구한다.
      */
 
     cin.tie(nullptr)->sync_with_stdio(false);
     cout.tie(nullptr)->sync_with_stdio(false);
-
+    
     cin >> n >> m >> k;
     for(int i = 0; i < n; i++) {
+        fill(dist[i], dist[i] + m, NOT_VISIT);
+        fill(mane_dist[i], mane_dist[i] + m, NOT_VISIT);
         for(int j = 0; j < m; j++) {
             cin >> board[i][j];
+
             if(board[i][j] == 4)
                 sp = dir(i, j);
             else if(board[i][j] == 3) {
-                board[i][j] = 1;
-                mannequin.push_back(dir(i, j));
+                mane_dist[i][j] = 0;
+                que.push(dir(i, j));
             }
         }
     }
 
+    while(!que.empty()) {
+        dir cur = que.front();
+        que.pop();
+
+        for(const dir& pd: poss_dir) {
+            dir nxt = cur + pd;
+            if(in_range(nxt) && mane_dist[nxt.row][nxt.col] == NOT_VISIT) {
+                mane_dist[nxt.row][nxt.col] = mane_dist[cur.row][cur.col] + 1;
+                que.push(nxt);
+            }
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(board[i][j] != 1 && mane_dist[i][j] <= k)
+                board[i][j] = 1;
+        }
+    }
+
     int ans = -1;
-    visit[sp.row][sp.col] = true;
-    que.push(qn(sp, 0));
+    board[sp.row][sp.col] = 0;
+    dist[sp.row][sp.col] = 0;
+    que.push(sp);
     while(!que.empty() && ans == -1) {
-        dir cur = que.front().pos;
-        int cur_dist = que.front().dist;
+        dir cur = que.front();
         que.pop();
 
         if(board[cur.row][cur.col] == 2)
-            ans = cur_dist;
+            ans = dist[cur.row][cur.col];
         else {
             for(const dir& pd: poss_dir) {
                 dir nxt = cur + pd;
-                if(in_range(nxt) && board[nxt.row][nxt.col] != 1 && far_manne(nxt) && !visit[nxt.row][nxt.col]) {
-                    visit[nxt.row][nxt.col] = true;
-                    que.push(qn(nxt, cur_dist + 1));
+                if(in_range(nxt) && board[nxt.row][nxt.col] != 1 && dist[nxt.row][nxt.col] == NOT_VISIT) {
+                    dist[nxt.row][nxt.col] = dist[cur.row][cur.col] + 1;
+                    que.push(nxt);
                 }
             }
         }
